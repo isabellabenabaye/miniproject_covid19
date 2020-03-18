5f3c3e6d538b034366fccf136cb75f2abe411dc1
Isabella Benabaye <isabellabenabaye@gmail.com>
2020-03-18 02:32:07 -0700

    Created an R project and the script to import from the reddit COVID19 case database

---
+++ ibenabaye/Import data from gsheet.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(rvest)
+
+#Import data from the r/Coronavirus_PH Google sheet
+url <- "https://docs.google.com/spreadsheets/d/1wdxIwD0b58znX4UrH6JJh_0IhnZP0YWn23Uqs7lHB6Q/gviz/tq?tqx=out:html&tq&gid=0"
+page <- read_html(url) #Creates an html document from URL
+table <- html_table(page, fill = TRUE) #Parses tables into data frames
+
+cases <- as_tibble(table[[1]])
+names(cases) <- c("case", "age", "sex", "nationality", "status1", "status2", "positive_date", "onset_date", "fever", "cough", "sore throat", "pneumonia", "symptoms_others", "other_conditions", "facility", "residence", "travel", "travel_history", "case_link", "link_desc", "details", "source", "column3", "column4", "column5", "column6", "column7", "column8", "column9", "column10")
+cases <- cases[-1,]
+cases <- cases[1:22]
+
+
