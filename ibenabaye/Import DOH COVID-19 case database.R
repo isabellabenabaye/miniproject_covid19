a809a3763fe07203265b712b47b9955308f39e36
Isabella Benabaye <isabellabenabaye@gmail.com>
2020-03-22 19:54:09 -0700

    adjust codes to new JSON file arrangement

--- ibenabaye/Import DOH COVID-19 case database.R
+++ ibenabaye/Import DOH COVID-19 case database.R
@@ -6,15 +6,19 @@ library(jsonlite)
 json_file <- "https://services5.arcgis.com/mnYJ21GiFTR97WFg/arcgis/rest/services/PH_masterlist/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=102100&cacheHint=true" ## url to the DOH tracker JSON file
 doh_raw <- fromJSON(json_file)
 cases_doh <- as_tibble(doh_raw$features$attributes) ## make the table
-names(cases_doh) <- c("case", "caseID", "age", "sex", "nationality", "residence", "travel_history", "symptoms", "positive_date", "facility", "latitude", "longitude", "status", "link_desc", "updated","x","y") ## properly label the columns
+cases_doh <- cases_doh[, 2:16]
+names(cases_doh) <- c("case", "caseID", "age", "sex", "nationality", "residence", "travel_history", "symptoms", "positive_date", "facility", "latitude", "longitude", "status", "link_desc", "updated") ## properly label the columns
 cases_doh
 
 # Clean Data
 cases_doh <- cases_doh %>%  
-  mutate_at(vars(names(cases_doh)), na_if, " ") %>% ## make blank values NA
-  mutate_at(vars(names(cases_doh)), na_if, "For validation") %>%
-  mutate_at(vars(names(cases_doh)), na_if,  "For Validation") %>%
-  mutate(positive_date = mdy(positive_date), ## format dates
-         updated = mdy_hm(updated),
+  mutate_all(function (x) ifelse(x %in% c(" ","-", "?", "For Validation","For validation"), NA, x)) %>%  ## replace with NA
+  
+  separate(updated, c("updated", "update_time"), sep = " ") %>% 
+  
+  ## format columns
+  mutate(case = as.character(case),
+         positive_date = mdy(positive_date), ## format dates
+         updated = mdy(updated),
          sex = as.factor(sex), ## factor sex and nationality
          nationality = as.factor(nationality))
