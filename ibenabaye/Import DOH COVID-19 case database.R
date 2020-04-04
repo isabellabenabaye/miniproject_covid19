319d7a6635a93be5f5201fc395f3e18a23eb958c
Patricia Rose Donato <PDonato@SavvyAsia.com>
2020-04-05 07:43:17 +0800

    changed first few lines to raw_doh

--- ibenabaye/Import DOH COVID-19 case database.R
+++ ibenabaye/Import DOH COVID-19 case database.R
@@ -6,11 +6,11 @@ library(lubridate)
 # Prepare Data
 ## import data from the official PH DOH tracker
 json_file <- "https://services5.arcgis.com/mnYJ21GiFTR97WFg/arcgis/rest/services/PH_masterlist/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=102100&cacheHint=true" ## url to the DOH tracker JSON file
-doh_raw <- fromJSON(json_file)
-cases_doh <- as_tibble(doh_raw$features$attributes) ## make the table
-cases_doh <- cases_doh[, 2:16]
-names(cases_doh) <- c("case", "caseID", "age", "sex", "nationality", "residence", "travel_history", "symptoms", "positive_date", "facility", "latitude", "longitude", "status", "link_desc", "updated") ## properly label the columns
-raw_doh <- cases_doh
+raw_doh <- fromJSON(json_file)
+raw_doh <- as_tibble(doh_raw$features$attributes) ## make the table
+raw_doh <- cases_doh[, 2:16]
+names(raw_doh) <- c("case", "caseID", "age", "sex", "nationality", "residence", "travel_history", "symptoms", "positive_date", "facility", "latitude", "longitude", "status", "link_desc", "updated") ## properly label the columns
+
 
 
 # Clean Data
